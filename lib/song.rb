5d0bc146c2104c0a676457204ddd3fe02fdf64b3
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-28 19:19:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,54 @@
 require 'pry'
+class Song 
+  attr_accessor :name, :artist, :genre
+  @@count
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artist << artist
+    @@genre << genre
+    @@count += 1
 
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists 
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    
+  end
+
+  def self.artist_count
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 class Song
 
   attr_accessor :name, :artist, :genre
